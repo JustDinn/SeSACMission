2f1a7fcae427c4087cf224aaeecf35f2466f38c9
HyoTaek <kht1790@gmail.com>
2025-07-11 21:44:10 +0900

    [UI] 여행 매거진 TableViewCell 초기 UI 구현
    - 셀 내부 컴포넌트의 오토 레이아웃 설정
    - magazineCell의 identifier 설정 및 스토리보드 연결
    - MagazineTableViewController에서 셀 개수, 셀 등록, 셀 높이 설정

--- Week1&Week2/SeSACMission11/SeSACMission11/Source/25.07.11/ViewController/TravelMagazineTableViewController.swift
+++ Week1&Week2/SeSACMission11/SeSACMission11/Source/25.07.11/ViewController/TravelMagazineTableViewController.swift
@@ -15,3 +15,18 @@ final class TravelMagazineTableViewController: UITableViewController {
     }
 }
 
+extension TravelMagazineTableViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "magazineCell", for: indexPath)
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 400
+    }
+}

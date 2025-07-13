3c3c6d30c9d1c375bf560f8ca6a99da18e95571b
HyoTaek <kht1790@gmail.com>
2025-07-13 23:56:03 +0900

    [UI] TravelInfo TableViewController 초기 생성

---
+++ Week1&Week2/SeSACMission11/SeSACMission11/Source/25.07.11/CityInfo/ViewController/TravelInfoTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TravelInfoTableViewController.swift
+//  SeSACMission11
+//
+//  Created by HyoTaek on 7/13/25.
+//
+
+import UIKit
+
+class TravelInfoTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    // MARK: - UITableview DataSource
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}

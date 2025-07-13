611ff1fcee65b49924545100cb12051603a42b8b
HyoTaek <kht1790@gmail.com>
2025-07-13 23:17:31 +0900

    [Feat] 1부터 입력한 숫자까지 textView의 text로 삽입

--- Week1&Week2/SeSACMission11/SeSACMission11/Source/25.07.11/369/ViewController/ThreeSixNineViewController.swift
+++ Week1&Week2/SeSACMission11/SeSACMission11/Source/25.07.11/369/ViewController/ThreeSixNineViewController.swift
@@ -37,4 +37,31 @@ class ThreeSixNineViewController: UIViewController {
     func setTextView() {
         resultTextView.isEditable = false
     }
+    
+    func setTextView(with number: Int) {
+        var wholeText = ""
+        
+        for i in 1...number {
+            if i != number {
+                wholeText += "\(i), "
+            } else {
+                wholeText += "\(i)"
+            }
+        }
+        resultTextView.text = wholeText
+    }
+    
+    // MARK: - Action
+    
+    @IBAction func didEndEditing(_ sender: UITextField) {
+        if let textNumber = numberInputTextField.text {
+            if let intNumber = Int(textNumber) {
+                setTextView(with: intNumber)
+            } else {
+                print("<< 정수 변환 실패")
+            }
+        } else {
+            print("<< 텍스트필드 옵셔널 바인딩 실패")
+        }
+    }
 }

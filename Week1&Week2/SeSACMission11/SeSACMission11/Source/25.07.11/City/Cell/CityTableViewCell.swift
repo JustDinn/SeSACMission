7d5148373ae39602811d24532230b809c59635d6
HyoTaek <kht1790@gmail.com>
2025-07-16 00:31:54 +0900

    [Feat] city cell에 description 데이터 적용

--- Week1&Week2/SeSACMission11/SeSACMission11/Source/25.07.11/City/Cell/CityTableViewCell.swift
+++ Week1&Week2/SeSACMission11/SeSACMission11/Source/25.07.11/City/Cell/CityTableViewCell.swift
@@ -19,6 +19,7 @@ class CityTableViewCell: UITableViewCell {
     @IBOutlet var containerView: UIView!
     @IBOutlet var backgroundImageView: UIImageView!
     @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
     
     // MARK: - Life Cycle
     
@@ -28,6 +29,7 @@ class CityTableViewCell: UITableViewCell {
         setContainerViewUI()
         setImageView()
         setTitleLabel()
+        setDescriptionLabel()
     }
     
     // MARK: - Set ContainerView UI
@@ -52,10 +54,19 @@ class CityTableViewCell: UITableViewCell {
         titleLabel.textAlignment = .right
     }
     
+    // MARK: - Set DescriptionLabel
+    
+    func setDescriptionLabel() {
+        descriptionLabel.font = UIFont.systemFont(ofSize: 12, weight: .light)
+        descriptionLabel.textColor = .white
+        descriptionLabel.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5)
+    }
+    
     // MARK: - Configure Cell
     
     func configureCell(with cityInfo: City) {
         backgroundImageView.kf.setImage(with: URL(string: cityInfo.city_image))
         titleLabel.text = "\(cityInfo.city_name) | \(cityInfo.city_english_name)"
+        descriptionLabel.text = cityInfo.city_explain
     }
 }

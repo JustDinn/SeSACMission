c5fd679aa4029b352e5f43673250bf81ccdf2003
HyoTaek <kht1790@gmail.com>
2025-07-15 21:25:12 +0900

    [UI] backButton UI 설정

--- Week1&Week2/SeSACMission11/SeSACMission11/Source/25.07.11/CityInfo/ViewController/TravelAdViewController.swift
+++ Week1&Week2/SeSACMission11/SeSACMission11/Source/25.07.11/CityInfo/ViewController/TravelAdViewController.swift
@@ -16,16 +16,15 @@ class TravelAdViewController: UIViewController {
     // MARK: - Components
     
     @IBOutlet var adTitleLabel: UILabel!
+    @IBOutlet var backButton: UIBarButtonItem!
     
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        adTitleLabel.font = UIFont.systemFont(ofSize: 24, weight: .black)
-        adTitleLabel.textAlignment = .center
-        adTitleLabel.numberOfLines = 0
-        
+        setBackButton()
+        setLabel()
         setAdUI()
     }
     
@@ -35,5 +34,20 @@ class TravelAdViewController: UIViewController {
         if let ad = adInfo {
             adTitleLabel.text = ad.title
         }
+    }
+    
+    // MARK: - Set Back Button
+    
+    func setBackButton() {
+        backButton.image = UIImage(systemName: "xmark")
+        backButton.tintColor = .black
+    }
+    
+    // MARK: - Set Label
+    
+    func setLabel() {
+        adTitleLabel.font = UIFont.systemFont(ofSize: 24, weight: .black)
+        adTitleLabel.textAlignment = .center
+        adTitleLabel.numberOfLines = 0
     }
 }

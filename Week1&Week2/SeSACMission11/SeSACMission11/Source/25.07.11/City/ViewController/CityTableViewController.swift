6b4e982f9e1440a3904eefefb995504411250394
HyoTaek <kht1790@gmail.com>
2025-07-16 01:04:09 +0900

    [Feat] 세그먼트를 이용해서 국내, 해외 여행지 필터링

--- Week1&Week2/SeSACMission11/SeSACMission11/Source/25.07.11/City/ViewController/CityTableViewController.swift
+++ Week1&Week2/SeSACMission11/SeSACMission11/Source/25.07.11/City/ViewController/CityTableViewController.swift
@@ -12,11 +12,24 @@ class CityTableViewController: UITableViewController {
     // MARK: - Properties
     
     let cityInfos = CityInfo().city
+    var filteredCities = CityInfo().city
+    
+    // MARK: - Components
+    
+    @IBOutlet var filterSegment: UISegmentedControl!
+    
+    // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         registerNib()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        filteredCities = cityInfos
     }
     
     // MARK: - Register Nib
@@ -27,18 +40,46 @@ class CityTableViewController: UITableViewController {
         tableView.register(cityXib, forCellReuseIdentifier: CityTableViewCell.identifier)
         tableView.separatorStyle = .none
     }
+    
+    // MARK: - Set Segment
+    
+    func setSegment() {
+        
+    }
+    
+    // MARK: - Action
+    
+    @IBAction func didSelectSegment(_ sender: UISegmentedControl) {
+        filteredCities.removeAll()
+        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            filteredCities = cityInfos
+            
+        case 1:
+            filteredCities = cityInfos.filter { $0.domestic_travel }
+            
+        case 2:
+            filteredCities = cityInfos.filter { !$0.domestic_travel }
+            
+        default:
+            fatalError("잘못된 세그먼트 인덱스")
+        }
+        
+        tableView.reloadData()
+    }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cityInfos.count
+        return filteredCities.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: CityTableViewCell.identifier, for: indexPath) as! CityTableViewCell
         let cellIndex = indexPath.row
         
-        cell.configureCell(with: cityInfos[cellIndex])
+        cell.configureCell(with: filteredCities[cellIndex])
         
         return cell
     }

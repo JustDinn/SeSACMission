878533f5805413e9f9638133fef96c94b57462bd
HyoTaek <kht1790@gmail.com>
2025-07-15 23:33:20 +0900

    [Feat] 도시 정보 tableViewController, tableViewCell 초기 생성

---
+++ Week1&Week2/SeSACMission11/SeSACMission11/Source/25.07.11/City/Cell/CityTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  CityTableViewCell.swift
+//  SeSACMission11
+//
+//  Created by HyoTaek on 7/15/25.
+//
+
+import UIKit
+
+class CityTableViewCell: UITableViewCell {
+
+    // MARK: - Properties
+    
+    static let identifier = "CityTableViewCell"
+    
+    // MARK: - Life Cycle
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
---
+++ Week1&Week2/SeSACMission11/SeSACMission11/Source/25.07.11/City/ViewController/CityTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  CityTableViewController.swift
+//  SeSACMission11
+//
+//  Created by HyoTaek on 7/15/25.
+//
+
+import UIKit
+
+class CityTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        registerNib()
+    }
+    
+    // MARK: - Register Nib
+    
+    func registerNib() {
+        let cityXib = UINib(nibName: CityTableViewCell.identifier, bundle: nil)
+        
+        tableView.register(cityXib, forCellReuseIdentifier: CityTableViewCell.identifier)
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CityTableViewCell.identifier, for: indexPath) as! CityTableViewCell
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+}

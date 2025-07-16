12b24f0599ce9ecb44a004cee739ce7fc2c8845f
HyoTaek <kht1790@gmail.com>
2025-07-16 22:46:10 +0900

    [Refactor] CityTableViewController -> CityViewController로 수정

---
+++ Week1&Week2/SeSACMission11/SeSACMission11/Source/25.07.11/City/ViewController/CityViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CityViewController.swift
+//  SeSACMission11
+//
+//  Created by HyoTaek on 7/16/25.
+//
+
+import UIKit
+
+class CityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}

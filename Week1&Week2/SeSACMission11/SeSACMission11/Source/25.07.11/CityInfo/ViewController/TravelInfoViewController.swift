641b741bc32f015e9bd4a653b08fec92c722356f
HyoTaek <kht1790@gmail.com>
2025-07-16 22:05:20 +0900

    [Feat] TravelInfoViewController 생성

---
+++ Week1&Week2/SeSACMission11/SeSACMission11/Source/25.07.11/CityInfo/ViewController/TravelInfoViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TravelInfoViewController.swift
+//  SeSACMission11
+//
+//  Created by HyoTaek on 7/16/25.
+//
+
+import UIKit
+
+class TravelInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}

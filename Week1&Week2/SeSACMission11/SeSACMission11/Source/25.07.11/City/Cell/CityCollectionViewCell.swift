22a2ecb2d16fff34f0c537129c41f40a504fa0b4
HyoTaek <kht1790@gmail.com>
2025-07-17 22:38:20 +0900

    [Refactor] collectionView로 리팩토링

---
+++ Week1&Week2/SeSACMission11/SeSACMission11/Source/25.07.11/City/Cell/CityCollectionViewCell.swift
@@ -0,0 +1,71 @@
+//
+//  CityCollectionViewCell.swift
+//  SeSACMission11
+//
+//  Created by HyoTaek on 7/17/25.
+//
+
+import UIKit
+
+class CityCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    
+    static let identifier = "CityTableViewCell"
+    
+    // MARK: - Components
+    
+    @IBOutlet var containerView: UIView!
+    @IBOutlet var backgroundImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    // MARK: - Life Cycle
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        setContainerViewUI()
+        setImageView()
+        setTitleLabel()
+        setDescriptionLabel()
+    }
+    
+    // MARK: - Set ContainerView UI
+    
+    func setContainerViewUI() {
+        containerView.layer.cornerRadius = 20
+        containerView.layer.masksToBounds = true
+        containerView.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMinYCorner, .layerMaxXMaxYCorner)
+    }
+    
+    // MARK: - Set ImageView
+    
+    func setImageView() {
+        backgroundImageView.contentMode = .scaleAspectFill
+    }
+    
+    // MARK: - Set TitleLabel
+    
+    func setTitleLabel() {
+        titleLabel.font = UIFont.systemFont(ofSize: 16, weight: .black)
+        titleLabel.textColor = .white
+        titleLabel.textAlignment = .right
+    }
+    
+    // MARK: - Set DescriptionLabel
+    
+    func setDescriptionLabel() {
+        descriptionLabel.font = UIFont.systemFont(ofSize: 12, weight: .light)
+        descriptionLabel.textColor = .white
+        descriptionLabel.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5)
+    }
+    
+    // MARK: - Configure Cell
+    
+    func configureCell(with cityInfo: City) {
+        backgroundImageView.kf.setImage(with: URL(string: cityInfo.city_image))
+        titleLabel.text = "\(cityInfo.city_name) | \(cityInfo.city_english_name)"
+        descriptionLabel.text = cityInfo.city_explain
+    }
+}
---
+++ Week1&Week2/SeSACMission11/SeSACMission11/Source/25.07.11/City/ViewController/CityCollectionViewController.swift
@@ -0,0 +1,143 @@
+//
+//  CityCollectionViewController.swift
+//  SeSACMission11
+//
+//  Created by HyoTaek on 7/17/25.
+//
+
+import UIKit
+
+class CityCollectionViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    let cityInfos = CityInfo().city
+    var filteredCities = CityInfo().city
+    
+    // MARK: - Components
+    
+//    @IBOutlet var tableView: UITableView!
+    @IBOutlet var filterSegment: UISegmentedControl!
+    @IBOutlet var searchTextField: UITextField!
+    @IBOutlet var collectionView: UICollectionView!
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        filteredCities = cityInfos
+        registerNib()
+    }
+    
+    // MARK: - Register Nib
+    
+    func registerNib() {
+//        let cityXib = UINib(nibName: CityCollectionViewCell.identifier, bundle: nil)
+//
+//        let nib = UINib(nibName: "FriendCollectionViewCell", bundle: nil)
+//        let layout = UICollectionViewFlowLayout()
+//        let width = (UIScreen.main.bounds.width - (16 * 2) - (16 * 3)) / 4
+//        
+//        layout.itemSize = CGSize(width: width, height: width)
+//        layout.sectionInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+//        layout.minimumInteritemSpacing = 16
+//        layout.minimumLineSpacing = 16
+//        layout.scrollDirection = .vertical
+//        
+//        collectionView.register(nib, forCellWithReuseIdentifier: FriendCollectionViewCell.identifier)
+//        collectionView.dataSource = self
+//        collectionView.delegate = self
+        
+//        collectionView.register(cityXib, forCellReuseIdentifier: CityTableViewCell.identifier)
+//        tableView.dataSource = self
+//        tableView.delegate = self
+//        tableView.separatorStyle = .none
+    }
+    
+    // MARK: - 필터
+    
+    func filter() {
+        didSelectSegment(filterSegment)
+        
+        if !searchTextField.text!.isEmpty {
+            filterCities()
+        }
+    }
+    
+    // MARK: - 여행지 필터링
+    
+    func filterCities() {
+        let searchingKeyword = searchTextField.text!.trimmingCharacters(in: .whitespaces).uppercased()
+        
+        filteredCities = filteredCities.filter {
+            $0.city_name.contains(searchingKeyword) ||
+            $0.city_english_name.contains(searchingKeyword) ||
+            $0.city_explain.contains(searchingKeyword)
+        }
+    }
+    
+    // MARK: - Action
+    
+    @IBAction func didSelectSegment(_ sender: UISegmentedControl) {
+        filteredCities.removeAll()
+        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            filteredCities = cityInfos
+            
+        case 1:
+            filteredCities = cityInfos.filter { $0.domestic_travel }
+            
+        case 2:
+            filteredCities = cityInfos.filter { !$0.domestic_travel }
+            
+        default:
+            fatalError("잘못된 세그먼트 인덱스")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    @IBAction func didEndEditingTextField(_ sender: UITextField) {
+        filter()
+        tableView.reloadData()
+    }
+    
+    @IBAction func didChangedTextField(_ sender: UITextField) {
+        filter()
+        tableView.reloadData()
+    }
+}
+
+//// MARK: - UICollectionView DataSource, Delegate
+//
+//extension CityCollectionViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return filteredCities.count
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: <#T##String#>, for: <#T##IndexPath#>)
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: CityTableViewCell.identifier, for: indexPath) as! CityTableViewCell
+//        let cellIndex = indexPath.row
+//        
+//        cell.configureCell(with: filteredCities[cellIndex])
+//        
+//        return cell
+//    }
+//    
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 160
+//    }
+//    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        let cityDetailVC = self.storyboard?.instantiateViewController(withIdentifier: CityDetailViewController.identifier) as! CityDetailViewController
+//        cityDetailVC.cityInfo = cityInfos[indexPath.row]
+//        
+//        present(cityDetailVC, animated: true)
+//    }
+//}

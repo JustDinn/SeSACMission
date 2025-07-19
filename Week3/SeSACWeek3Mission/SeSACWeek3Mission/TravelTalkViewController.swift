59ad5708bbb86d9d89ff2c1aedfdc584c2a13ddc
HyoTaek <kht1790@gmail.com>
2025-07-20 01:27:04 +0900

    [Refactor] TravelTalkViewController 초기 구현
    - TravelTalkViewController를 final class로 변경
    - searchBar를 private으로 변경
    - searchBar 설정

--- Week3/SeSACWeek3Mission/SeSACWeek3Mission/TravelTalkViewController.swift
+++ Week3/SeSACWeek3Mission/SeSACWeek3Mission/TravelTalkViewController.swift
@@ -7,14 +7,22 @@
 
 import UIKit
 
-class TravelTalkViewController: UIViewController {
+final class TravelTalkViewController: UIViewController {
 
+    @IBOutlet private var searchBar: UISearchBar!
     
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setSearchBar()
     }
     
+    // MARK: - 검색바 설정
+    
+    private func setSearchBar() {
+        searchBar.placeholder = "친구 이름을 검색해보세요"
+        searchBar.searchBarStyle = .minimal
+    }
 }

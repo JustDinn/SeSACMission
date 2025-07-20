71ada1ec01a4d48c6214d0fead13667747a5771b
HyoTaek <kht1790@gmail.com>
2025-07-20 23:36:36 +0900

    [Feat] 채팅 데이터 추가

---
+++ Week3/SeSACWeek3Mission/SeSACWeek3Mission/Chat.swift
@@ -0,0 +1,15 @@
+//
+//  Chat.swift
+//  SeSACWeek3Mission
+//
+//  Created by HyoTaek on 7/20/25.
+//
+
+import Foundation
+
+//채팅 화면에서 사용할 데이터 구조체
+struct Chat {
+    let user: User
+    let date: String
+    let message: String
+}
---
+++ Week3/SeSACWeek3Mission/SeSACWeek3Mission/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  SeSACWeek3Mission
+//
+//  Created by HyoTaek on 7/20/25.
+//
+
+import Foundation
+
+struct User {
+    let name: String //유저 닉네임
+    let image: String //유저 프로필
+}

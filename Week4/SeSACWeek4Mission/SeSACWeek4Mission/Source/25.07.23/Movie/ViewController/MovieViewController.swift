c457acb0672d9460ba3afc3566f5e0b413804525
HyoTaek <kht1790@gmail.com>
2025-07-24 02:54:36 +0900

    [UI] 영화 검색 텍스트필드 UI 설정

--- Week4/SeSACWeek4Mission/SeSACWeek4Mission/Source/25.07.23/Movie/ViewController/MovieViewController.swift
+++ Week4/SeSACWeek4Mission/SeSACWeek4Mission/Source/25.07.23/Movie/ViewController/MovieViewController.swift
@@ -6,13 +6,49 @@
 //
 
 import UIKit
+import SnapKit
 
-final class MovieViewController: UIViewController {
+final class MovieViewController: UIViewController, InitialSetProtocol {
 
+    // MARK: - Component
+    
+    private lazy var searchTextField: UITextField = {
+        let textField = UITextField()
+        
+        textField.placeholder = "영화 검색"
+        textField.textColor = .white
+        
+        return textField
+    }()
+    
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setUI()
+        setAddView()
+        setConstraints()
+    }
+    
+    // MARK: - Set UI
+    
+    func setUI() {
+        view.backgroundColor = .black
+    }
+    
+    func setAddView() {
+        [
+            searchTextField
+        ].forEach(view.addSubview)
+    }
+    
+    func setConstraints() {
+        searchTextField.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(40)
+            $0.leading.equalToSuperview().offset(16)
+            $0.width.equalTo(UIScreen.main.bounds.width * 0.7)
+            $0.height.equalTo(44)
+        }
     }
 }

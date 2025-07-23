4afa9b8164883ad25988c5f843785973f8df2cc8
HyoTaek <kht1790@gmail.com>
2025-07-24 02:41:13 +0900

    [Feat] MovieVC 생성

---
+++ Week4/SeSACWeek4Mission/SeSACWeek4Mission/Common/Extension/UIViewController++Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIViewController++Extension.swift
+//  SeSACWeek4Mission
+//
+//  Created by HyoTaek on 7/24/25.
+//
+
+import Foundation
---
+++ Week4/SeSACWeek4Mission/SeSACWeek4Mission/Source/25.07.23/Movie/ViewController/MovieViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MovieViewController.swift
+//  SeSACWeek4Mission
+//
+//  Created by HyoTaek on 7/24/25.
+//
+
+import UIKit
+
+final class MovieViewController: UIViewController {
+
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}

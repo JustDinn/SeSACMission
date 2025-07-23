dc8e4d9a9164b75d499c914cd5b143f2a0edae43
HyoTaek <kht1790@gmail.com>
2025-07-23 23:46:26 +0900

    [Feat] 기본 세팅 프로토콜 구현

---
+++ Week4/SeSACWeek4Mission/SeSACWeek4Mission/Source/25.07.23/Protocol/InitialSetProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  InitialSetProtocol.swift
+//  SeSACWeek4Mission
+//
+//  Created by HyoTaek on 7/23/25.
+//
+
+import Foundation
+
+protocol InitialSetProtocol: AnyObject {
+    
+    func setUI()
+    func setAddView()
+    func setConstraints()
+}

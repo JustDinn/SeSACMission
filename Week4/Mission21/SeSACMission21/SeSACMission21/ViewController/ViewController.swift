9fde3fd97b62887ac201655b89b8c717db08782e
HyoTaek <kht1790@gmail.com>
2025-07-26 09:12:51 +0900

    [Feat] 미션21 프로젝트 생성 및 초기 설정
    - 스토리보드 삭제 및 navigationController 등록
    - 폴더 구조 설정

---
+++ Week4/Mission21/SeSACMission21/SeSACMission21/ViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACMission21
+//
+//  Created by HyoTaek on 7/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

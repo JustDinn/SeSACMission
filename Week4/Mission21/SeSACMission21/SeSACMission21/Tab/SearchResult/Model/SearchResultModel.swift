919e175f99ef9f703ef2fe7883d66d3386d6b6c2
HyoTaek <kht1790@gmail.com>
2025-07-26 12:31:47 +0900

    [Networking] 네이버 쇼핑 API 디코딩 모델 구현

---
+++ Week4/Mission21/SeSACMission21/SeSACMission21/Tab/SearchResult/Model/SearchResultModel.swift
@@ -0,0 +1,22 @@
+//
+//  SearchResultModel.swift
+//  SeSACMission21
+//
+//  Created by HyoTaek on 7/26/25.
+//
+
+import Foundation
+
+struct SearchResultModel: Decodable {
+    let itemImage: String
+    let mallName: String
+    let title: String
+    let itemPrice: String
+    
+    enum CodingKeys: String, CodingKey {
+        case itemImage = "image"
+        case mallName
+        case title
+        case itemPrice = "lprice"
+    }
+}

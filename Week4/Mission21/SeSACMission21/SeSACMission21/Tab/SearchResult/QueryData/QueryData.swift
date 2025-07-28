9505366c68d5ffa759e2c7e642a7a4956958a799
HyoTaek <kht1790@gmail.com>
2025-07-29 02:19:02 +0900

    [Feat] 쿼리에 들어갈 데이터를 싱글톤 패턴으로 관리

--- Week4/Mission21/SeSACMission21/SeSACMission21/Tab/SearchResult/QueryData/QueryData.swift
+++ Week4/Mission21/SeSACMission21/SeSACMission21/Tab/SearchResult/QueryData/QueryData.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+final class QueryData {
+    static let shared = QueryData()
+    
+    var keyword: String?
+    var sort = "sim"
+    var pageNumber = 1
+    var pageSize = 30
+    
+    private init() { }
+}

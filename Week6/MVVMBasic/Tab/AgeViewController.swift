9aa43ad074df5f7fe9b89e03fd05bb787d3a5271
HyoTaek <kht1790@gmail.com>
2025-08-07 22:07:01 +0900

    [Feat] 코드 정리 및 마크다운 추가

--- Week6/MVVMBasic/Tab/AgeViewController.swift
+++ Week6/MVVMBasic/Tab/AgeViewController.swift
@@ -6,43 +6,50 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
-class AgeViewController: UIViewController {
-    let textField: UITextField = {
-        let textField = UITextField()
-        textField.placeholder = "나이를 입력해주세요"
-        textField.borderStyle = .roundedRect
-        return textField
-    }()
-    let resultButton: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .systemBlue
-        button.setTitle( "클릭", for: .normal)
-        button.layer.cornerRadius = 8
-        return button
-    }()
-    let label: UILabel = {
-        let label = UILabel()
-        label.text = "여기에 결과를 보여주세요"
-        label.textAlignment = .center
-        return label
-    }()
+final class AgeViewController: UIViewController {
+    
+    // MARK: - Component
+    
+    private let textField = UITextField().then {
+        $0.placeholder = "나이를 입력해주세요"
+        $0.borderStyle = .roundedRect
+    }
+    
+    private lazy var resultButton = UIButton().then {
+        $0.backgroundColor = .systemBlue
+        $0.setTitle( "클릭", for: .normal)
+        $0.layer.cornerRadius = 8
+        $0.addTarget(self, action: #selector(resultButtonTapped), for: .touchUpInside)
+    }
+    
+    private let label = UILabel().then {
+        $0.text = "여기에 결과를 보여주세요"
+        $0.textAlignment = .center
+    }
+    
+    // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         configureHierarchy()
         configureLayout()
-        
-        resultButton.addTarget(self, action: #selector(resultButtonTapped), for: .touchUpInside)
     }
     
-    func configureHierarchy() {
+    // MARK: - Set Hierarchy
+    
+    private func configureHierarchy() {
         view.addSubview(textField)
         view.addSubview(resultButton)
         view.addSubview(label)
     }
     
-    func configureLayout() {
+    // MARK: - Set Constraints
+    
+    private func configureLayout() {
         textField.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
             make.horizontalEdges.equalToSuperview().inset(20)
@@ -61,6 +68,8 @@ class AgeViewController: UIViewController {
             make.height.equalTo(44)
         }
     }
+    
+    // MARK: - Action
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         view.endEditing(true)

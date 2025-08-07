5b740f08fee4f0318f35a8e7fe6c9a8c250cca7e
HyoTaek <kht1790@gmail.com>
2025-08-08 00:06:43 +0900

    [Feat] String -> Date 함수 구현

---
+++ Week6/MVVMBasic/Extension/String++Extension.swift
@@ -0,0 +1,20 @@
+//
+//  String++Extension.swift
+//  MVVMBasic
+//
+//  Created by HyoTaek on 8/7/25.
+//
+
+import Foundation
+
+extension String {
+    
+    func toDate(format: String = "yyyy.MM.dd") -> Date? {
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        dateFormatter.dateFormat = format
+        
+        return dateFormatter.date(from: self)
+    }
+}

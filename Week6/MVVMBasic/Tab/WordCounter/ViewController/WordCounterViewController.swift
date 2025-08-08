05225451828f5f6b91c77610e42edfaedc601b47
HyoTaek <kht1790@gmail.com>
2025-08-08 14:46:51 +0900

    [Feat] WordCounterViewModel 생성
    - 글자수 업데이트 함수 구현
    - text: wordCounterVC로부터 전달받는 textView.text
    - resultMessage: 글자수 계산 결과 메세지를 vc에 전달

---
+++ Week6/MVVMBasic/Tab/WordCounter/ViewController/WordCounterViewController.swift
@@ -0,0 +1,87 @@
+//
+//  WordCounterViewController.swift
+//  SeSACSevenWeek
+//
+//  Created by Jack on 2/5/25.
+//
+
+import UIKit
+import SnapKit
+ 
+final class WordCounterViewController: UIViewController {
+    
+    // MARK: - Component
+    
+    private lazy var textView: UITextView = {
+        let textView = UITextView()
+        textView.font = .systemFont(ofSize: 16)
+        textView.layer.borderWidth = 1
+        textView.layer.borderColor = UIColor.systemGray4.cgColor
+        textView.layer.cornerRadius = 8
+        textView.textContainerInset = UIEdgeInsets(top: 12, left: 8, bottom: 12, right: 8)
+        textView.delegate = self
+        return textView
+    }()
+    
+    private let countLabel: UILabel = {
+        let label = UILabel()
+        label.text = "현재까지 0글자 작성중"
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 16, weight: .medium)
+        label.textColor = .systemBlue
+        return label
+    }()
+     
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+        setupConstraints()
+    }
+     
+    // MARK: - Set UI
+    
+    private func setupUI() {
+        view.backgroundColor = .white
+        
+        [
+            textView,
+            countLabel
+        ].forEach(view.addSubview)
+    }
+    
+    // MARK: - Set Constraints
+    
+    private func setupConstraints() {
+        countLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.height.equalTo(30)
+        }
+        
+        textView.snp.makeConstraints { make in
+            make.top.equalTo(countLabel.snp.bottom).offset(16)
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.height.equalTo(view.snp.width)
+        }
+    }
+     
+    // MARK: - Update CharacterCount
+    
+    private func updateCharacterCount() {
+        let count = textView.text.count
+        countLabel.text = "현재까지 \(count)글자 작성중"
+    }
+}
+ 
+// MARK: - UITextView Delegate
+
+extension WordCounterViewController: UITextViewDelegate {
+    
+    // 텍스트뷰 입력값 변경
+    func textViewDidChange(_ textView: UITextView) {
+        updateCharacterCount()
+    }
+}
---
+++ Week6/MVVMBasic/Tab/WordCounter/ViewModel/WordCounterViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  WordCounterViewModel.swift
+//  MVVMBasic
+//
+//  Created by HyoTaek on 8/8/25.
+//
+
+import Foundation
+
+final class WordCounterViewModel {
+    
+    // MARK: - Property
+    
+    var text: String? = "" {
+        didSet {
+            updateCharacterCount()
+        }
+    }
+    
+    var resultMessage = ""
+    
+    // MARK: - Update CharacterCount
+    
+    private func updateCharacterCount() {
+        guard let text else {
+            resultMessage = "입력한 글자에 문제가 생겼습니다"
+            return
+        }
+        
+        let count = text.count
+        resultMessage = "현재까지 \(count)글자 작성중"
+    }
+}

570f7fb507f651cdd2f8b7554ff93cc5034ccd9c
HyoTaek <kht1790@gmail.com>
2025-08-11 00:56:54 +0900

    [Refactor] BirthDayVC에 BirthDayViewModel 연결

--- Week6/MVVMBasic/Tab/BirthDay/ViewController/BirthDayViewController.swift
+++ Week6/MVVMBasic/Tab/BirthDay/ViewController/BirthDayViewController.swift
@@ -11,6 +11,10 @@ import Then
 
 final class BirthDayViewController: UIViewController {
     
+    // MARK: - Property
+    
+    private let birthDayViewModel = BirthDayViewModel()
+    
     // MARK: - Component
     
     private let yearTextField = UITextField().then {
@@ -59,6 +63,7 @@ final class BirthDayViewController: UIViewController {
         
         configureHierarchy()
         configureLayout()
+        bindClosure()
     }
     
     // MARK: - Set Hierarchy
@@ -132,117 +137,18 @@ final class BirthDayViewController: UIViewController {
     }
     
     @objc func resultButtonTapped() {
-        do {
-            try isValidDate()
-            let inputDate = "\(yearTextField.text!).\(monthTextField.text!).\(dateTextField.text!)"
-            guard let toDate = inputDate.toDate() else {
-                return
-            }
-            let dateGap = dateGap(toDate: toDate)
-            
-            resultLabel.text = "오늘 날짜 기준으로 \(dateGap)일째입니다"
-        } catch {
-            switch error {
-            case .notIntYear:
-                resultLabel.text = "입력한 연도를 정수로 변환할 수 없음"
-            case .outOfYear:
-                resultLabel.text = "입력 가능한 연도의 범위를 벗어남"
-            case .notIntMonth:
-                resultLabel.text = "입력한 월을 정수로 변환할 수 없음"
-            case .outOfMonth:
-                resultLabel.text = "입력 가능한 월의 범위를 벗어남"
-            case .notIntDate:
-                resultLabel.text = "입력한 일을 정수로 변환할 수 없음"
-            case .outOfDate:
-                resultLabel.text = "입력 가능한 일의 범위를 벗어남"
-            }
-        }
+        birthDayViewModel.year = yearTextField.text
+        birthDayViewModel.month = monthTextField.text
+        birthDayViewModel.date = dateTextField.text
         
         view.endEditing(true)
     }
     
-    // MARK: - 날짜 유효성 검사
+    // MARK: - Bind Closure
     
-    @discardableResult
-    private func isValidDate() throws(DateError) -> Bool {
-        guard let year = yearTextField.text,
-              let month = monthTextField.text,
-              let date = dateTextField.text else {
-            return false
+    private func bindClosure() {
+        birthDayViewModel.result = { message in
+            self.resultLabel.text = message
         }
-        
-        // 연도 유효성 검사
-        if Int(year) == nil {
-            throw .notIntYear
-        }
-        
-        if Int(year)! < 0 {
-            throw .outOfYear
-        }
-        
-        // 월 유효성 검사
-        if Int(month) == nil {
-            throw .notIntMonth
-        }
-        
-        // 범위 비교는 한개와 비교하기 때문에 시간복잡도가 O(1)이지만, contains는 배열의 모두와 비고하기 떄문에 시간복잡도가 O(1)보다 큼.
-        // 코드 시간복잡도 개선
-        if Int(month)! < 1 && Int(month)! > 12 {
-            throw .outOfMonth
-        }
-//        if !(1...12).contains(Int(month)!) {
-//            throw .outOfMonth
-//        }
-        
-        // 일 유효성 검사
-        if Int(date) == nil {
-            throw .notIntDate
-        }
-        
-        switch Int(month)! {
-        case 1, 3, 5, 7, 8, 10, 12:
-            if !(1...31).contains(Int(date)!) {
-                throw .outOfDate
-            }
-        case 4, 6, 9, 11:
-            if !(1...30).contains(Int(date)!) {
-                throw .outOfDate
-            }
-        case 2:
-            if isLeapYear(Int(year)!) && !(1...29).contains(Int(date)!) {
-                throw .outOfDate
-            } else if !isLeapYear(Int(year)!) && !(1...28).contains(Int(date)!) {
-                throw .outOfDate
-            }
-        default:
-            return false
-        }
-        return true
-    }
-    
-    // MARK: - 윤년 판별
-    
-    func isLeapYear(_ year: Int) -> Bool {
-        
-        if year % 400 == 0 {
-            return true
-        } else if year % 100 != 0 && year % 4 == 0 {
-            return true
-        } else {
-            return false
-        }
-    }
-    
-    // MARK: - 날짜 차이 구하기
-    
-    func dateGap(toDate: Date) -> Int {
-        let calendar = Calendar.current
-        
-        let toDate = calendar.startOfDay(for: toDate)
-        let today = calendar.startOfDay(for: .now)
-        
-        let gap = calendar.dateComponents([.day], from: today, to: toDate).day ?? 0
-        
-        return gap
     }
 }

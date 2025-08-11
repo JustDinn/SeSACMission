ba5413a40ff561c5d2c1dd5a15317c98318f733c
HyoTaek <kht1790@gmail.com>
2025-08-12 02:12:02 +0900

    [Feat] 지도에 중식만 필터링

--- Week6/MVVMBasic/Tab/Map/MapViewController.swift
+++ Week6/MVVMBasic/Tab/Map/MapViewController.swift
@@ -125,8 +125,25 @@ class MapViewController: UIViewController {
             self.mapView.showAnnotations(annotations, animated: true)
         }
         
-        let alert3Action = UIAlertAction(title: "얼럿 3", style: .default) { _ in
-            print("얼럿 3이 선택되었습니다.")
+        let alert3Action = UIAlertAction(title: "중식", style: .default) { _ in
+            self.mapView.removeAnnotations(self.mapView.annotations)
+            
+            var annotations: [MKPointAnnotation] = []
+            
+            self.restaurantList.filter { $0.category == "중식" }.forEach {
+                let annotation = MKPointAnnotation()
+                
+                annotation.coordinate = CLLocationCoordinate2D(
+                    latitude: $0.latitude,
+                    longitude: $0.longitude
+                )
+                annotation.title = $0.name
+                annotation.subtitle = "\($0.category) | \($0.price)원"
+                
+                annotations.append(annotation)
+            }
+            self.mapView.addAnnotations(annotations)
+            self.mapView.showAnnotations(annotations, animated: true)
         }
         
         let cancelAction = UIAlertAction(title: "취소", style: .cancel) { _ in

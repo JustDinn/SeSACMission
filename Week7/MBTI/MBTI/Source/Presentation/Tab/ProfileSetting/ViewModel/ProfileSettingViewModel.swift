fe626a2b7b1e486d7c4c32442bda3e5fb609404a
HyoTaek <kht1790@gmail.com>
2025-08-13 23:00:21 +0900

    [Feat] 프로필 설정viewModel 생성
    - 이미지 배열 생성함수 구현 및 사용

--- Week7/MBTI/MBTI/Source/Presentation/Tab/ProfileSetting/ViewModel/ProfileSettingViewModel.swift
+++ Week7/MBTI/MBTI/Source/Presentation/Tab/ProfileSetting/ViewModel/ProfileSettingViewModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+final class ProfileSettingViewModel {
+    
+    // MARK: - Property
+    
+    private var randomImageList: [String] = []
+    
+    // MARK: - Init
+    
+    init() {
+        makeRandomImageList()
+    }
+    
+    // MARK: - Method
+    
+    // 이미지 배열 생성
+    private func makeRandomImageList() {
+        for index in 1...12 {
+            randomImageList.append("person\(index)")
+        }
+    }
+}

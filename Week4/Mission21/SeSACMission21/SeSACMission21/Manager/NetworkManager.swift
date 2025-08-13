55ef88d7442f231ad63c676911139a6d5bbf56b2
HyoTaek <kht1790@gmail.com>
2025-08-13 22:08:21 +0900

    [Refactor] NetworkManager의 함수를 제네릭으로 구성
    - Search API 호출뿐만 아니라 다른 api 요청도 받을 수 있도록 구현

--- Week4/Mission21/SeSACMission21/SeSACMission21/Manager/NetworkManager.swift
+++ Week4/Mission21/SeSACMission21/SeSACMission21/Manager/NetworkManager.swift
@@ -14,30 +14,34 @@ final class NetworkManager {
     
     private init() { }
     
-    // 네이버 쇼핑 검색 API Get 요청
-    func searchKeyword(api: NetworkRouter, isRecommendSearching: Bool = false, completion: @escaping (Result<Search, Error>) -> Void) {
+    // API 요청 공통 함수
+    func networkRequest<T: Decodable>(
+        api: NetworkRouter,
+        isRecommendSearching: Bool = false,
+        model: T.Type,
+        completion: @escaping (Result<T, Error>) -> Void) {
         
-        AF.request(api.endpoint, method: api.method, parameters: api.parameters, headers: api.headers)
-            .validate(statusCode: 200..<300)
-            .responseDecodable(of: Search.self) { response in
-                switch response.result {
-                case .success(let searchResult):
-                    completion(.success(searchResult))
-                    
-                case .failure(let error):
-                    if let statusCode = response.response?.statusCode {
-                        completion(.failure(NetworkError.responseFail(statusCode: statusCode, errorMessage: error.localizedDescription)))
-                    }
-                    // 200번대 성공 응답을 받았지만, 모델로 디코딩하는 과정에서 실패한 경우
-                    // 예: 서버가 다른 형식의 JSON을 보냈거나, 필수 필드가 누락된 경우
-                    else if error.isResponseSerializationError {
-                        completion(.failure(NetworkError.decodingFailed(errorMessage: error.localizedDescription)))
-                    }
-                    // 명시적으로 처리하지 않은 응답 실패 경우
-                    else {
-                        completion(.failure(NetworkError.unknownError))
+            AF.request(api.endpoint, method: api.method, parameters: api.parameters, headers: api.headers)
+                .validate(statusCode: 200..<300)
+                .responseDecodable(of: model.self) { response in
+                    switch response.result {
+                    case .success(let searchResult):
+                        completion(.success(searchResult))
+                        
+                    case .failure(let error):
+                        if let statusCode = response.response?.statusCode {
+                            completion(.failure(NetworkError.responseFail(statusCode: statusCode, errorMessage: error.localizedDescription)))
+                        }
+                        // 200번대 성공 응답을 받았지만, 모델로 디코딩하는 과정에서 실패한 경우
+                        // 예: 서버가 다른 형식의 JSON을 보냈거나, 필수 필드가 누락된 경우
+                        else if error.isResponseSerializationError {
+                            completion(.failure(NetworkError.decodingFailed(errorMessage: error.localizedDescription)))
+                        }
+                        // 명시적으로 처리하지 않은 응답 실패 경우
+                        else {
+                            completion(.failure(NetworkError.unknownError))
+                        }
                     }
                 }
-            }
     }
 }
--- Week4/Mission21/SeSACMission21/SeSACMission21/Tab/SearchResult/ViewModel/SearchResultViewModel.swift
+++ Week4/Mission21/SeSACMission21/SeSACMission21/Tab/SearchResult/ViewModel/SearchResultViewModel.swift
@@ -90,84 +90,84 @@ final class SearchResultViewModel {
                         : queryData.searchedHistory.randomElement() ?? "가오리"
         
         // 네이버 쇼핑 검색 API 호출
-        networkManager.searchKeyword(
+        networkManager.networkRequest(
             api: .search(
                 query: query,
                 pageSize: queryData.pageSize.formatted(),
                 sort: queryData.sort,
                 pageNumber: queryData.pageNumber.formatted()
             ),
-            isRecommendSearching: isRecommendSearching
-        ) { result in
-            switch result {
-            case .success(let searchedResult):
-                // 첫 API 호출시에만 마지막 페이지 개수 계산
-                if self.queryData.lastPage == nil {
-                    self.queryData.lastPage = Int(ceil(Double(searchedResult.totalCount) / Double(self.queryData.pageSize)))
-                }
-                self.output.searchedCount.value = searchedResult.totalCount
-                
-                // 최대 페이지 설정
-                if let lastPage = self.queryData.lastPage {
-                    let pageNumber = self.queryData.pageNumber
+            isRecommendSearching: isRecommendSearching,
+            model: Search.self) { result in
+                switch result {
+                case .success(let searchedResult):
+                    // 첫 API 호출시에만 마지막 페이지 개수 계산
+                    if self.queryData.lastPage == nil {
+                        self.queryData.lastPage = Int(ceil(Double(searchedResult.totalCount) / Double(self.queryData.pageSize)))
+                    }
+                    self.output.searchedCount.value = searchedResult.totalCount
                     
-                    if pageNumber <= min(lastPage, 1000) {
-                        if isRecommendSearching {
-                            self.output.recommendResult.value.append(contentsOf: searchedResult.items)
-                        } else {
-                            self.output.searchedResult.value.append(contentsOf: searchedResult.items)
+                    // 최대 페이지 설정
+                    if let lastPage = self.queryData.lastPage {
+                        let pageNumber = self.queryData.pageNumber
+                        
+                        if pageNumber <= min(lastPage, 1000) {
+                            if isRecommendSearching {
+                                self.output.recommendResult.value.append(contentsOf: searchedResult.items)
+                            } else {
+                                self.output.searchedResult.value.append(contentsOf: searchedResult.items)
+                            }
                         }
                     }
-                }
-                
-            case .failure(let error):
-                var message: String
-                
-                if let networkError = error as? NetworkError {
-                    switch networkError {
-                    case .responseFail(let statusCode, let errorMessage):
-                        print("<< 네이버 쇼핑 검색 API 응답 실패 - 상태코드: \(statusCode), 메시지: \(errorMessage ?? "없음")")
-                        
-                        switch statusCode {
-                        case 400:
-                            print("<< 네이버 쇼핑 검색 API: 잘못된 요청")
-                            message = "[\(statusCode)] 잘못된 요청입니다"
+                    
+                case .failure(let error):
+                    var message: String
+                    
+                    if let networkError = error as? NetworkError {
+                        switch networkError {
+                        case .responseFail(let statusCode, let errorMessage):
+                            print("<< 네이버 쇼핑 검색 API 응답 실패 - 상태코드: \(statusCode), 메시지: \(errorMessage ?? "없음")")
                             
-                        case 401:
-                            print("<< 네이버 쇼핑 검색 API: 인증 실패")
-                            message = "[\(statusCode)] 인증 실패(API Key, Cliend ID 누락 혹은 만료)"
+                            switch statusCode {
+                            case 400:
+                                print("<< 네이버 쇼핑 검색 API: 잘못된 요청")
+                                message = "[\(statusCode)] 잘못된 요청입니다"
+                                
+                            case 401:
+                                print("<< 네이버 쇼핑 검색 API: 인증 실패")
+                                message = "[\(statusCode)] 인증 실패(API Key, Cliend ID 누락 혹은 만료)"
+                                
+                            case 403:
+                                print("<< 네이버 쇼핑 검색 API: 접근 거부")
+                                message = "[\(statusCode)] 접근 거부"
+                                
+                            case 404:
+                                print("<< 네이버 쇼핑 검색 API: 찾을 수 없음")
+                                message = "[\(statusCode)] 서버 찾을 수 없음"
+                                
+                            case 500...599:
+                                print("<< 네이버 쇼핑 검색 API: 서버 오류")
+                                message = "[\(statusCode)] 서버 에러"
+                                
+                            default:
+                                print("<< 네이버 쇼핑 검색 API: 기타 오류")
+                                message = "[\(statusCode)] 기타 에러"
+                            }
                             
-                        case 403:
-                            print("<< 네이버 쇼핑 검색 API: 접근 거부")
-                            message = "[\(statusCode)] 접근 거부"
-                        
-                        case 404:
-                            print("<< 네이버 쇼핑 검색 API: 찾을 수 없음")
-                            message = "[\(statusCode)] 서버 찾을 수 없음"
+                        case .decodingFailed(let errorMessage):
+                            print("<< 네이버 쇼핑 검색 API: 디코딩 실패 \(errorMessage)")
+                            message = "디코딩 에러"
                             
-                        case 500...599:
-                            print("<< 네이버 쇼핑 검색 API: 서버 오류")
-                            message = "[\(statusCode)] 서버 에러"
-
-                        default:
-                            print("<< 네이버 쇼핑 검색 API: 기타 오류")
-                            message = "[\(statusCode)] 기타 에러"
+                        case .unknownError:
+                            print("<< 알 수 없는 네트워크 에러")
+                            message = "알 수 없는 네트워크 에러"
                         }
-                        
-                    case .decodingFailed(let errorMessage):
-                        print("<< 네이버 쇼핑 검색 API: 디코딩 실패 \(errorMessage)")
-                        message = "디코딩 에러"
-                        
-                    case .unknownError:
-                        print("<< 알 수 없는 네트워크 에러")
-                        message = "알 수 없는 네트워크 에러"
+                    } else {
+                        print("<< 알 수 없는 에러: \(error.localizedDescription)")
+                        message = "알 수 없는 에러"
                     }
-                } else {
-                    print("<< 알 수 없는 에러: \(error.localizedDescription)")
-                    message = "알 수 없는 에러"
+                    self.output.errorMessage.value = message
                 }
-                self.output.errorMessage.value = message
-            }
         }
     }
 }

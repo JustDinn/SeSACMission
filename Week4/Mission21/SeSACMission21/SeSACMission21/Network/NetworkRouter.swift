14b7fbba5a222d53f557017c456186151128399c
HyoTaek <kht1790@gmail.com>
2025-08-13 16:00:21 +0900

    [Refactor] NetworkRouter 생성
    - baseURL을 secret에 저장 후 불러옴

---
+++ Week4/Mission21/SeSACMission21/SeSACMission21/Network/NetworkRouter.swift
@@ -0,0 +1,20 @@
+//
+//  NetworkRouter.swift
+//  SeSACMission21
+//
+//  Created by HyoTaek on 8/13/25.
+//
+
+import Foundation
+import Alamofire
+
+enum NetworkRouter {
+    case search
+    
+    var baseURL: String {
+        guard let url = Bundle.main.infoDictionary?["BASE_URL"] as? String else {
+            return ""
+        }
+        return url
+    }
+}

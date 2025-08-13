2bd593dc59ed510e42e222590b28cf37d464d146
HyoTaek <kht1790@gmail.com>
2025-08-13 16:28:44 +0900

    [Refactor] 라우터 패턴에 파라미터 추가

--- Week4/Mission21/SeSACMission21/SeSACMission21/Network/NetworkRouter.swift
+++ Week4/Mission21/SeSACMission21/SeSACMission21/Network/NetworkRouter.swift
@@ -9,7 +9,9 @@ import Foundation
 import Alamofire
 
 enum NetworkRouter {
-    case search
+    case search(String, Int, String, Int)
+    
+    // MARK: - Base URL
     
     var baseURL: String {
         guard let url = Bundle.main.infoDictionary?["BASE_URL"] as? String else {
@@ -18,17 +20,35 @@ enum NetworkRouter {
         return url
     }
     
+    // MARK: - Endpoint
+    
     var endpoint: URL {
         switch self {
         case .search:
-            return URL(string: "search/shop.json")!
+            return URL(string: baseURL + "search/shop.json")!
         }
     }
+    
+    // MARK: - Method
     
     var method: HTTPMethod {
         switch self {
         case .search:
             return .get
+        }
+    }
+    
+    // MARK: - Parameters
+    
+    var parameters: Parameters {
+        switch self {
+        case .search(let query, let pageSize, let sort, let pageNumber):
+            return [
+                "query": query,
+                "display": pageSize,
+                "sort": sort,
+                "start": pageNumber
+            ]
         }
     }
 }

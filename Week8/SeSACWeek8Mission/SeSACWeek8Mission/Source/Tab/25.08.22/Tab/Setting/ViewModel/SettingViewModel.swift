d2988a97538ef02b2333591c6d98081883172745
HyoTaek <kht1790@gmail.com>
2025-08-23 01:05:51 +0900

    [Feat] SettingViewModel 생성
    - cellTitle: 설정 페이지의 셀 타이틀을 초기 설정

--- Week8/SeSACWeek8Mission/SeSACWeek8Mission/Source/Tab/25.08.22/Tab/Setting/ViewModel/SettingViewModel.swift
+++ Week8/SeSACWeek8Mission/SeSACWeek8Mission/Source/Tab/25.08.22/Tab/Setting/ViewModel/SettingViewModel.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+final class SettingViewModel {
+    
+//    // MARK: - DisposeBag
+//    
+//    private let disposeBag = DisposeBag()
+//    
+//    // MARK: - Input
+//    
+//    struct Input {
+//        
+//    }
+    
+    // MARK: - Output
+    
+    struct Output {
+        let cellTitle: BehaviorSubject<[String]>
+    }
+    
+    // MARK: - Init
+    
+    // MARK: - Transform
+    
+    func transform() -> Output {
+        let settingData = BehaviorSubject(value: ["내 이름 설정하기", "다마고치 변경하기", "데이터 초기화"])
+        
+        return Output(cellTitle: settingData)
+    }
+}

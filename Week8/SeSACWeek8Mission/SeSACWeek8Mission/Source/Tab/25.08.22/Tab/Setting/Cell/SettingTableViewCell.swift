f967c577be5a370af435efb439337776ddaf84b2
HyoTaek <kht1790@gmail.com>
2025-08-23 01:05:10 +0900

    [UI] SettingTableViewCell UI 생성

--- Week8/SeSACWeek8Mission/SeSACWeek8Mission/Source/Tab/25.08.22/Tab/Setting/Cell/SettingTableViewCell.swift
+++ Week8/SeSACWeek8Mission/SeSACWeek8Mission/Source/Tab/25.08.22/Tab/Setting/Cell/SettingTableViewCell.swift
@@ -6,18 +6,45 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
-class SettingTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+final class SettingTableViewCell: UITableViewCell, ReuseIdentifier {
+    
+    // MARK: - Component
+    
+    private let settingButton = UIButton().then {
+        $0.setTitle("test", for: .normal)
+        $0.setTitleColor(.black, for: .normal)
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    // MARK: - Init
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setHierarchy()
+        setConstraints()
     }
-
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Set Hierarchy
+    
+    private func setHierarchy() {
+        [
+            settingButton
+        ].forEach(contentView.addSubview)
+    }
+    
+    // MARK: - Set Constraints
+    
+    private func setConstraints() {
+        settingButton.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
 }

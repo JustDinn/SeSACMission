8c009523c53d7ce55970683f4de2458d968e9c66
HyoTaek <kht1790@gmail.com>
2025-08-23 23:29:32 +0900

    [Feat] EditNicknameViewModel 생성
    - editNicknameVC의 저장 버튼의 액션을 input으로 받은 뒤 transform에서 처리하도록 설계

--- Week8/SeSACWeek8Mission/SeSACWeek8Mission/Source/Tab/25.08.22/Tab/EditNickname/ViewModel/EditNicknameViewModel.swift
+++ Week8/SeSACWeek8Mission/SeSACWeek8Mission/Source/Tab/25.08.22/Tab/EditNickname/ViewModel/EditNicknameViewModel.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+final class EditNicknameViewModel {
+    
+    // MARK: - DisposeBag
+    
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - Input
+    
+    struct Input {
+        let saveButtonTapped: ControlEvent<Void>
+    }
+    
+    // MARK: - Output
+    
+    struct Output {
+        
+    }
+    
+    // MARK: - Transform
+    
+    func transform(input: Input) -> Output {
+        input.saveButtonTapped
+            .bind(with: self) { owner, _ in
+                
+            }
+            .disposed(by: disposeBag)
+        
+        return Output()
+    }
+}

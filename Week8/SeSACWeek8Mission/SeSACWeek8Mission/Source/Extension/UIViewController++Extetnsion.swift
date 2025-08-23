cf8e8d6aac6d290300b794c28842fa204bc005c4
HyoTaek <kht1790@gmail.com>
2025-08-23 21:25:57 +0900

    [Feat] UIViewController Extension 구현
    - 타이틀만 있는 네비바 생성 함수 구현

--- Week8/SeSACWeek8Mission/SeSACWeek8Mission/Source/Extension/UIViewController++Extetnsion.swift
+++ Week8/SeSACWeek8Mission/SeSACWeek8Mission/Source/Extension/UIViewController++Extetnsion.swift
@@ -5,4 +5,18 @@
 //  Created by HyoTaek on 8/23/25.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    // title만 있는 Navigation Bar
+    func setTitleNaviBar(_ title: String) {
+        let titleLabel = UILabel().then {
+            $0.setLabel(text: title, fontSize: 18, fontWeight: .black, textColor: .white)
+        }
+        
+        self.navigationItem.titleView = titleLabel
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: nil, style: .plain, target: nil, action: nil)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: nil, style: .plain, target: nil, action: nil)
+    }
+}

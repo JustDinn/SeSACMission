430840c344c457c59d3d920ee0e45497f8ef4bdd
HyoTaek <kht1790@gmail.com>
2025-08-25 00:51:03 +0900

    [UI] 다마고치 상세 정보 페이지 UI 구현

--- Week8/SeSACWeek8Mission/SeSACWeek8Mission/Source/Tab/25.08.22/Tab/SelectTamagotchi/ViewConotroller/TamagotchiDetailViewController.swift
+++ Week8/SeSACWeek8Mission/SeSACWeek8Mission/Source/Tab/25.08.22/Tab/SelectTamagotchi/ViewConotroller/TamagotchiDetailViewController.swift
@@ -11,17 +11,44 @@ import Then
 
 final class TamagotchiDetailViewController: UIViewController {
 
+    // MARK: - Component
+    
+    private let tamagotchiInfoView = UIView().then {
+        $0.backgroundColor = .main
+        $0.layer.cornerRadius = 10
+    }
+    
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setUI()
+        setHierarchy()
+        setConstraints()
     }
     
     // MARK: - Set UI
     
     private func setUI() {
         view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+    }
+    
+    // MARK: - Set Hierarchy
+    
+    private func setHierarchy() {
+        [
+            tamagotchiInfoView
+        ].forEach(view.addSubview)
+    }
+    
+    // MARK: - Set Constraints
+    
+    private func setConstraints() {
+        tamagotchiInfoView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.width.equalTo(300)
+            $0.height.equalTo(400)
+        }
     }
 }

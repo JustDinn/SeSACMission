a6a626d9c80378d90e0291ccbc9a93dd0c749e62
HyoTaek <kht1790@gmail.com>
2025-08-24 22:58:20 +0900

    [Feat] 다마고치 데이터 생성

--- Week8/SeSACWeek8Mission/SeSACWeek8Mission/Source/Tab/25.08.22/Tab/SelectTamagotchi/Model/TamagotchiModel.swift
+++ Week8/SeSACWeek8Mission/SeSACWeek8Mission/Source/Tab/25.08.22/Tab/SelectTamagotchi/Model/TamagotchiModel.swift
@@ -6,3 +6,91 @@
 //
 
 import Foundation
+
+struct TamagotchiModel {
+    let name: String
+    let image: String
+    
+    static let tamagotchies: [TamagotchiModel] = [
+        TamagotchiModel(
+            name: "따끔따끔 다마고치",
+            image: ""
+        ),
+        TamagotchiModel(
+            name: "방실방실 다마고치",
+            image: ""
+        ),
+        TamagotchiModel(
+            name: "반짝반짝 다마고치",
+            image: ""
+        ),
+        TamagotchiModel(
+            name: "준비중이에요",
+            image: ""
+        ),
+        TamagotchiModel(
+            name: "준비중이에요",
+            image: ""
+        ),
+        TamagotchiModel(
+            name: "준비중이에요",
+            image: ""
+        ),
+        TamagotchiModel(
+            name: "준비중이에요",
+            image: ""
+        ),
+        TamagotchiModel(
+            name: "준비중이에요",
+            image: ""
+        ),
+        TamagotchiModel(
+            name: "준비중이에요",
+            image: ""
+        ),
+        TamagotchiModel(
+            name: "준비중이에요",
+            image: ""
+        ),
+        TamagotchiModel(
+            name: "준비중이에요",
+            image: ""
+        ),
+        TamagotchiModel(
+            name: "준비중이에요",
+            image: ""
+        ),
+        TamagotchiModel(
+            name: "준비중이에요",
+            image: ""
+        ),
+        TamagotchiModel(
+            name: "준비중이에요",
+            image: ""
+        ),
+        TamagotchiModel(
+            name: "준비중이에요",
+            image: ""
+        ),
+        TamagotchiModel(
+            name: "준비중이에요",
+            image: ""
+        ),
+        TamagotchiModel(
+            name: "준비중이에요",
+            image: ""
+        ),
+        TamagotchiModel(
+            name: "준비중이에요",
+            image: ""
+        ),
+        TamagotchiModel(
+            name: "준비중이에요",
+            image: ""
+        ),
+        TamagotchiModel(
+            name: "준비중이에요",
+            image: ""
+        )
+    ]
+}

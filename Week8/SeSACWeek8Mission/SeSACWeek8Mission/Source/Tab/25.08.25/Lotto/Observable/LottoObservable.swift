5b80443f620028e599d3dccd784073d2a4cbb742
HyoTaek <kht1790@gmail.com>
2025-08-26 01:35:41 +0900

    [Networking] 로또 커스텀 옵저버블 생성
    - 로또 API 연결 함수 구현

--- Week8/SeSACWeek8Mission/SeSACWeek8Mission/Source/Tab/25.08.25/Lotto/Observable/LottoObservable.swift
+++ Week8/SeSACWeek8Mission/SeSACWeek8Mission/Source/Tab/25.08.25/Lotto/Observable/LottoObservable.swift
@@ -5,4 +5,33 @@
 //  Created by HyoTaek on 8/26/25.
 //
 
-import Foundation
+import RxSwift
+import RxCocoa
+import Alamofire
+
+enum LottoError: Error {
+    case invalid
+}
+
+final class LottoObservable {
+    
+    static func getLottoNumber(query: String) -> Observable<LottoModel> {
+        return Observable<LottoModel>.create { observer in
+            let endpoint = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=\(query)"
+            
+            AF.request(endpoint, method: .get)
+                .responseDecodable(of: LottoModel.self) { response in
+                    switch response.result {
+                    case .success(let lottoNumbers):
+                        observer.onNext(lottoNumbers)
+                        observer.onCompleted()
+                        
+                    case .failure(let error):
+                        observer.onError(error)
+                    }
+                }
+            
+            return Disposables.create()
+        }
+    }
+}

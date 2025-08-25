a6b0e62736a11cd640db9ad4d6034b4401da61a8
HyoTaek <kht1790@gmail.com>
2025-08-26 03:43:04 +0900

    [Networking] 박스오피스 옵저버블 생성 및 API 연결 함수 구현

--- Week8/SeSACWeek8Mission/SeSACWeek8Mission/Source/Tab/25.08.25/BoxOffice/Observable/BoxOfficeObservable.swift
+++ Week8/SeSACWeek8Mission/SeSACWeek8Mission/Source/Tab/25.08.25/BoxOffice/Observable/BoxOfficeObservable.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import Alamofire
+
+enum BoxOfficeError: Error {
+    case Invalid
+}
+
+final class BoxOfficeObservable {
+    
+    static func getBoxOfficeInfo(query: String) -> Observable<MovieModel> {
+        return Observable<MovieModel>.create { observer in
+            guard let boxOfficeAPIKey = Bundle.main.infoDictionary?["BOX_OFFICE_API_KEY"] as? String else {
+                return Disposables.create()
+            }
+            let url = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=\(boxOfficeAPIKey)&targetDt=\(query)"
+            
+            AF.request(url, method: .get)
+                .responseDecodable(of: MovieModel.self) { response in
+                    switch response.result {
+                    case .success(let boxOfficeInfo):
+                        observer.onNext(boxOfficeInfo)
+                        observer.onCompleted()
+                        
+                    case .failure(let error):
+                        observer.onError(error)
+                    }
+                }
+            
+            return Disposables.create()
+        }
+    }
+}

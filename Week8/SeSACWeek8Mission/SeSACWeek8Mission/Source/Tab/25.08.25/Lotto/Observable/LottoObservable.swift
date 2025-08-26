cdb74873192d40b6a6035442c462b4b1823be671
HyoTaek <kht1790@gmail.com>
2025-08-26 19:26:27 +0900

    [Refactor] LottoObservable 리팩토링
    - 반환값을 Result타입으로 변경
    - 성공, 실패 모두 onNext에 Result타입의 success, failure를 담아서 옵저버블 반환

--- Week8/SeSACWeek8Mission/SeSACWeek8Mission/Source/Tab/25.08.25/Lotto/Observable/LottoObservable.swift
+++ Week8/SeSACWeek8Mission/SeSACWeek8Mission/Source/Tab/25.08.25/Lotto/Observable/LottoObservable.swift
@@ -15,19 +15,20 @@ enum LottoError: Error {
 
 final class LottoObservable {
     
-    static func getLottoNumber(query: String) -> Observable<LottoModel> {
-        return Observable<LottoModel>.create { observer in
+    static func getLottoNumber(query: String) -> Observable<Result<LottoModel, LottoError>> {
+        return Observable<Result<LottoModel, LottoError>>.create { observer in
             let endpoint = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=\(query)"
             
             AF.request(endpoint, method: .get)
                 .responseDecodable(of: LottoModel.self) { response in
                     switch response.result {
                     case .success(let lottoNumbers):
-                        observer.onNext(lottoNumbers)
+                        observer.onNext(.success(lottoNumbers))
                         observer.onCompleted()
                         
-                    case .failure(let error):
-                        observer.onError(error)
+                    case .failure:
+                        observer.onNext(.failure(.invalid))
+                        observer.onCompleted()
                     }
                 }
             

a25e3eb2baee6cdd1d43ef5380f1913266ced4ac
HyoTaek <kht1790@gmail.com>
2025-08-26 23:19:32 +0900

    [Feat] 에러 메세지 Output 추가

--- Week8/SeSACWeek8Mission/SeSACWeek8Mission/Source/Tab/25.08.25/BoxOffice/ViewModel/BoxOfficeViewModel.swift
+++ Week8/SeSACWeek8Mission/SeSACWeek8Mission/Source/Tab/25.08.25/BoxOffice/ViewModel/BoxOfficeViewModel.swift
@@ -28,12 +28,14 @@ final class BoxOfficeViewModel: BaseViewModel {
     
     struct Output {
         let result: PublishRelay<[MovieInfo]>
+        let errorMessage: PublishRelay<String>
     }
     
     // MARK: - Transform
     
     func transform(input: In) -> Out {
         let result = PublishRelay<[MovieInfo]>()
+        let errorMessage = PublishRelay<String>()
         
         input.searchButtonTapped
             .withLatestFrom(input.searchKeyword)
@@ -41,15 +43,32 @@ final class BoxOfficeViewModel: BaseViewModel {
             .flatMap {
                 BoxOfficeObservable
                     .getBoxOfficeInfo(query: $0)
-                    .catch { _ in
+                    .catch { error in
                         return Observable.never()
                     }
             }
-            .bind(with: self) { owner, boxOfficeInfo in
-                result.accept(boxOfficeInfo.movie.movieList)
+            .bind(with: self) { owner, response in
+                switch response {
+                case .success(let boxOfficeInfo):
+                    result.accept(boxOfficeInfo.movie.movieList)
+                    
+                case .failure(let error):
+                    switch error {
+                    case .badRequest:
+                        errorMessage.accept("[400] 잘못된 요청입니다")
+                    case .unAuthorized:
+                        errorMessage.accept("[401] 토큰 만료 혹은 잘못된 토큰입니다")
+                    case .notFound:
+                        errorMessage.accept("[404] 요청받은 리소스를 찾을 수 없습니다")
+                    case .serverError:
+                        errorMessage.accept("[500] 서버 에러입니다")
+                    case .unknownError:
+                        errorMessage.accept("[Unknown] 알 수 없는 에러가 발생했습니다")
+                    }
+                }
             }
             .disposed(by: disposeBag)
         
-        return Out(result: result)
+        return Out(result: result, errorMessage: errorMessage)
     }
 }

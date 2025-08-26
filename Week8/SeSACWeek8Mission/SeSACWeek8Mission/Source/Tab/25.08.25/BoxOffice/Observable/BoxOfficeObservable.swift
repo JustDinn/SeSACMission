db34bc3763685d3045226675265e8cbb6ae5bd5a
HyoTaek <kht1790@gmail.com>
2025-08-26 23:18:28 +0900

    [Refactor] BoxOfficeObservable 리팩토링
    - 반환 타입을 Result타입으로 리팩토링
    - 엔드포인트를 Secret.xcconfig로 이동

--- Week8/SeSACWeek8Mission/SeSACWeek8Mission/Source/Tab/25.08.25/BoxOffice/Observable/BoxOfficeObservable.swift
+++ Week8/SeSACWeek8Mission/SeSACWeek8Mission/Source/Tab/25.08.25/BoxOffice/Observable/BoxOfficeObservable.swift
@@ -10,28 +10,41 @@ import RxSwift
 import RxCocoa
 import Alamofire
 
-enum BoxOfficeError: Error {
-    case Invalid
-}
-
 final class BoxOfficeObservable {
     
-    static func getBoxOfficeInfo(query: String) -> Observable<MovieModel> {
-        return Observable<MovieModel>.create { observer in
+    static func getBoxOfficeInfo(query: String) -> Observable<Result<MovieModel, NetworkError>> {
+        return Observable<Result<MovieModel, NetworkError>>.create { observer in
             guard let boxOfficeAPIKey = Bundle.main.infoDictionary?["BOX_OFFICE_API_KEY"] as? String else {
                 return Disposables.create()
             }
-            let url = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=\(boxOfficeAPIKey)&targetDt=\(query)"
+            guard let baseURL = Bundle.main.infoDictionary?["BASE_URL"] as? String else {
+                return Disposables.create()
+            }
+            let url = baseURL + "searchDailyBoxOfficeList.json?key=\(boxOfficeAPIKey)&targetDt=\(query)"
             
             AF.request(url, method: .get)
                 .responseDecodable(of: MovieModel.self) { response in
                     switch response.result {
                     case .success(let boxOfficeInfo):
-                        observer.onNext(boxOfficeInfo)
+                        observer.onNext(.success(boxOfficeInfo))
                         observer.onCompleted()
                         
                     case .failure(let error):
-                        observer.onError(error)
+                        let statusCode = error.responseCode
+                        
+                        switch statusCode {
+                        case 400:
+                            observer.onNext(.failure(.badRequest))
+                        case 401:
+                            observer.onNext(.failure(.unAuthorized))
+                        case 404:
+                            observer.onNext(.failure(.notFound))
+                        case 500:
+                            observer.onNext(.failure(.serverError))
+                        default:
+                            observer.onNext(.failure(.unknownError))
+                        }
+                        observer.onCompleted()
                     }
                 }
             

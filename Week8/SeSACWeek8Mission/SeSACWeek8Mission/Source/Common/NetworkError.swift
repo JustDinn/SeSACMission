66f49523bc9ea78486bb523a83df2f5d429844e5
HyoTaek <kht1790@gmail.com>
2025-08-26 22:29:20 +0900

    [Feat] NetworkError 정의

---
+++ Week8/SeSACWeek8Mission/SeSACWeek8Mission/Source/Common/NetworkError.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkError.swift
+//  SeSACWeek8Mission
+//
+//  Created by HyoTaek on 8/26/25.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case badRequest
+    case unAuthorized
+    case notFound
+    case serverError
+    case unknownError
+}

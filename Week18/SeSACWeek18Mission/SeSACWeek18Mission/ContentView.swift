d0c8e76aaece04d88c01d4afa84502aa0fcdf9f9
HyoTaek <kht1790@gmail.com>
2025-10-24 20:26:36 +0900

    [UI] 시도 횟수Text 추가

--- Week18/SeSACWeek18Mission/SeSACWeek18Mission/ContentView.swift
+++ Week18/SeSACWeek18Mission/SeSACWeek18Mission/ContentView.swift
@@ -12,17 +12,25 @@ struct ContentView: View {
     // MARK: - Property
     
     @State var number: String = ""
+    private var tryCounting: Int = 0
     
     // MARK: - Body
     
     var body: some View {
-        VStack {
+        VStack(spacing: 0) {
             Text("숫자 야구 게임")
                 .font(.title)
                 .fontWeight(.black)
                 .frame(maxWidth: .infinity)
                 .background(.white)
+            
             Spacer()
+            
+            Text("시도 횟수: \(tryCounting)회")
+                .frame(maxWidth: .infinity)
+                .padding(6)
+                .background(Color(uiColor: .systemGray4))
+            
             HStack(spacing: 10) {
                 TextField("3자리 숫자 입력", text: $number)
                     .padding(5)
@@ -43,5 +51,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView(number: "")
+    ContentView()
 }

7c1db9f4e711157d71d7a63f6de9f8aabedfe4bb
HyoTaek <kht1790@gmail.com>
2025-10-24 19:54:45 +0900

    [Feat] 새싹 18주차 과제 프로젝트 생성

---
+++ Week18/SeSACWeek18Mission/SeSACWeek18Mission/SeSACWeek18MissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeSACWeek18MissionApp.swift
+//  SeSACWeek18Mission
+//
+//  Created by HyoTaek on 10/24/25.
+//
+
+import SwiftUI
+
+@main
+struct SeSACWeek18MissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

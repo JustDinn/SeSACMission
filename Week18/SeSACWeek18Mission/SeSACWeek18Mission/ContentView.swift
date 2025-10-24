e7e681bddbd710a7451dda43b7a5e4de16128fc7
HyoTaek <kht1790@gmail.com>
2025-10-24 20:16:00 +0900

    [UI] 숫자 입력 텍스트필드, 확인 버튼 추가

--- Week18/SeSACWeek18Mission/SeSACWeek18Mission/ContentView.swift
+++ Week18/SeSACWeek18Mission/SeSACWeek18Mission/ContentView.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: - Property
+    
+    @State var number: String = ""
+    
+    // MARK: - Body
+    
     var body: some View {
         VStack {
             Text("숫자 야구 게임")
@@ -16,6 +23,18 @@ struct ContentView: View {
                 .frame(maxWidth: .infinity)
                 .background(.white)
             Spacer()
+            HStack(spacing: 10) {
+                TextField("3자리 숫자 입력", text: $number)
+                    .padding(5)
+                    .border(.gray, width: 1)
+                Button("확인") {
+                    print("확인 탭")
+                }
+                .padding(6)
+                .background(Color(uiColor: .systemGray4))
+            }
+            .padding()
+            .background(.white)
         }
         .frame(maxWidth: .infinity)
         .background(Color(uiColor: .systemGroupedBackground))
@@ -23,5 +42,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    ContentView(number: "")
 }
